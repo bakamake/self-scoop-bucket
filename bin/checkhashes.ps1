1c38379586b35684c563573df7caeced7a24ff68
bakamake <159852463+bakamake@users.noreply.github.com>
2025-03-20 23:33:14 +0800

    remove bin\... charge bucket/chatwise.json to v0.8.8

--- bin/checkhashes.ps1
+++
@@ -1,5 +0,0 @@
-#Requires -Version 5.1
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$checkhashes = "$env:SCOOP_HOME\bin\checkhashes.ps1"
-$dir = "$psscriptroot\..\bucket" # checks the parent dir
-Invoke-Expression -Command "$checkhashes -dir $dir $($args | ForEach-Object { "$_ " })"
